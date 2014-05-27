100e3bf91e8cf1ffd9b1e62b55a8392a1e28749a
Spurtle14 <eddes14@gmail.com>
2014-05-27 11:35:03 -0700

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
 console.log('This would be the main JS file.');
+
+var chatRef = new Firebase('https://luminous-fire-5993.firebaseio.com/');
+var auth = new FirebaseSimpleLogin(chatRef, function(error, user) {
+  if (error) {
+    // an error occurred while attempting login
+    console.log(error);
+  } else if (user) {
+    // user authenticated with Firebase
+    console.log('User ID: ' + user.uid + ', Provider: ' + user.provider);
+  } else {
+    // user is logged out
+  }
+});
