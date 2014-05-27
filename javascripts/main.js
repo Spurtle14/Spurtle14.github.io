a23914e0440f4c3663ddfc4aa8636188ab5e46f1
Spurtle14 <eddes14@gmail.com>
2014-05-27 11:38:44 -0700

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,6 +4,14 @@ console.log('This would be the main JS file.');
         src='https://cdn.firebase.com/js/simple-login/1.4.1/firebase-simple-login.js'>
 </script>
 
+
+auth.createUser(email, password, function(error, user) {
+  if (!error) {
+    console.log('User Id: ' + user.uid + ', Email: ' + user.email);
+  }
+});
+
+
 var chatRef = new Firebase('https://luminous-fire-5993.firebaseio.com/');
 var auth = new FirebaseSimpleLogin(chatRef, function(error, user) {
   if (error) {
